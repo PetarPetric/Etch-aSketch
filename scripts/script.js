0f342e4a1134731529f20314fa2703a8ab6f94bd
theperacar <petricpetar22@gmail.com>
2020-01-04 17:07:43 +0100

    Added hovering effect

--- scripts/script.js
+++ scripts/script.js
@@ -4,5 +4,16 @@ for ( let i = 0; i < 16*16 ; i++ ) {
   const newDivs = document.createElement('div');
   newDivs.classList.add('manyDivs');
   containerJS.appendChild(newDivs);
+
 }
+
+
+  let element = document.getElementsByClassName('manyDivs');
+
+  for(let i = 0; i < element.length; i++) {
+    element[i].addEventListener('mouseover', function(){
+      this.style.background = 'black';
+    });
+  }
+
 
