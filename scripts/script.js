0dc1d970a4e6d7e060f1ae2763ee92cdd538d7a2
theperacar <petricpetar22@gmail.com>
2020-01-06 00:14:55 +0100

    Added button funcionality that lets you chose number of boxes for drawing

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,35 @@
 let containerJS = document.getElementById('container');
 let element = document.getElementsByClassName('manyDivs');
 
-
-for ( let i = 0; i < 16*16 ; i++ ) {
+function createGrid(cellNumber) {
+for ( let i = 0; i < cellNumber*cellNumber ; i++ ) {
   const newDivs = document.createElement('div');
   newDivs.classList.add('manyDivs');
+  
+  //even listener added here
+  newDivs.addEventListener('mouseover', function(){
+  newDivs.style.background = 'black';});
   containerJS.appendChild(newDivs);
 
 }
 
+// dont need this can put EventListener 
+/*
 for(let i = 0; i < element.length; i++) {
   element[i].addEventListener('mouseover', function(){
     this.style.background = 'black';    
-  });
+    });
+  } */
 }
+createGrid(16);
 
-
-function newGrid() {
+function btnGrid() {
   let grid = window.prompt('Please enter grid size, maximum is 64');
-
+  let resetNew = document.getElementById('container');
+  let columnRowCSS = getComputedStyle(document.documentElement).getPropertyValue('--CSSgrid');
+  resetNew.innerHTML = "";
+  document.documentElement.style.setProperty('--CSSgrid', grid);
+  createGrid(grid);
 }
+
+document.querySelector('.new-grid').addEventListener('click', btnGrid);
