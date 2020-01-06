432d91ad065141447b299c99c809e07537241091
theperacar <petricpetar22@gmail.com>
2020-01-06 16:23:18 +0100

    Etch a sketch is working

--- scripts/script.js
+++ scripts/script.js
@@ -6,14 +6,14 @@ for ( let i = 0; i < cellNumber*cellNumber ; i++ ) {
   const newDivs = document.createElement('div');
   newDivs.classList.add('manyDivs');
   
-  //even listener added here
+  // Eventlistener added here
   newDivs.addEventListener('mouseover', function(){
   newDivs.style.background = 'black';});
   containerJS.appendChild(newDivs);
 
 }
 
-// dont need this can put EventListener 
+// Dont need this, can put EventListener up
 /*
 for(let i = 0; i < element.length; i++) {
   element[i].addEventListener('mouseover', function(){
@@ -21,6 +21,7 @@ for(let i = 0; i < element.length; i++) {
     });
   } */
 }
+
 createGrid(16);
 
 function btnGrid() {
@@ -33,3 +34,35 @@ function btnGrid() {
 }
 
 document.querySelector('.new-grid').addEventListener('click', btnGrid);
+
+// colorful grid function
+function getRandomRgb() {
+  let r = Math.floor(Math.random()* 255);
+  let g = Math.floor(Math.random()* 255);
+  let b = Math.floor(Math.random()* 255);
+  return 'rgb(' + r + ', ' + g + ', ' + b + ')'; 
+}
+
+//
+function createRGB(cellNumber) {
+  for ( let i = 0; i < cellNumber*cellNumber ; i++ ) {
+    const newDivs = document.createElement('div');
+    newDivs.classList.add('manyDivs');
+    //another event listener here
+    newDivs.addEventListener('mouseover', function(){
+    newDivs.style.background = getRandomRgb();
+    })
+    containerJS.appendChild(newDivs);
+  }
+}
+
+function btnColorful() {
+  let grid = window.prompt('Please enter grid size, maximum is 64');
+  let resetNew = document.getElementById('container');
+  let columnRowCSS = getComputedStyle(document.documentElement).getPropertyValue('--CSSgrid');
+  resetNew.innerHTML = "";
+  document.documentElement.style.setProperty('--CSSgrid', grid);
+  createRGB(grid);
+}
+
+document.querySelector('.new-colorful').addEventListener('click',btnColorful);
